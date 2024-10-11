4ac1f3ac4e182c86e6b32d26b112b9d94d2cfbd2
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-10-11 14:08:29 +0530

    add

--- Backend/routes/AuthRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { signUp } from "../controller/signUp.js";
-import { LoginValidation, signupValidation } from "../middleWares/validation.js";
-import login from "../controller/login.js";
-const route = Router();
-route.post('/login',LoginValidation,login);
-route.post('/signup',signupValidation,signUp);
-export default route;
